c92628dca096ec73630bb34d762a39102952d024
yinluobing <yinluobing@163.com>
2020-01-08 18:32:34 +0800

    [更新] ffmpege bash 迭代

--- config/bash/ffmpeg.sh
+++ config/bash/ffmpeg.sh
@@ -8,10 +8,10 @@ if [ ! -d "/opt/ffmpeg" ]; then
   mkdir /opt/ffmpeg
 fi
 
-cd /opt/ffmpeg \
-install wget \
-apt install wget \
-wget https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz \
-tar xvf ./ffmpeg-git-amd64-static.tar.xz --strip-components=1 \
-ln -s /opt/ffmpeg/ffmpeg /usr/bin/ffmpeg \
+cd /opt/ffmpeg
+install wget
+apt install wget
+wget https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
+tar xvf ./ffmpeg-git-amd64-static.tar.xz --strip-components=1
+ln -s /opt/ffmpeg/ffmpeg /usr/bin/ffmpeg
 ln -s /opt/ffmpeg/ffmpeg /usr/bin/ffprobe
